ddf5abd61bf9b2e62d5104151188e9aea4a8877c
s0153173 <sanvyawahare@gmail.com>
2020-11-05 18:44:49 +0100

    Changes in names of the agent

--- play.py
+++ play.py
@@ -6,10 +6,10 @@ BOARD_COLS = 3
 
 
 class State:
-    def __init__(self, p1, p2):
+    def __init__(self, ai_agent, ai_opponent):
         self.board = np.zeros((BOARD_ROWS, BOARD_COLS))
-        self.p1 = p1
-        self.p2 = p2
+        self.agent = ai_agent
+        self.opponent = ai_opponent
         self.isEnd = False
         self.boardHash = None
         # init p1 plays first
@@ -75,14 +75,14 @@ class State:
         result = self.winner()
         # backpropagate reward
         if result == 1:
-            self.p1.feedReward(1)
-            self.p2.feedReward(0)
+            self.agent.feedReward(1)
+            self.opponent.feedReward(0)
         elif result == -1:
-            self.p1.feedReward(0)
-            self.p2.feedReward(1)
+            self.agent.feedReward(0)
+            self.opponent.feedReward(1)
         else:
-            self.p1.feedReward(0.1)
-            self.p2.feedReward(0.5)
+            self.agent.feedReward(0.1)
+            self.opponent.feedReward(0.5)
 
     # board reset
     def reset(self):
@@ -98,11 +98,11 @@ class State:
             while not self.isEnd:
                 # Player 1
                 positions = self.availablePositions()
-                p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
+                p1_action = self.agent.chooseAction(positions, self.board, self.playerSymbol)
                 # take action and upate board state
                 self.updateState(p1_action)
                 board_hash = self.getHash()
-                self.p1.addState(board_hash)
+                self.agent.addState(board_hash)
                 # check board status if it is end
 
                 win = self.winner()
@@ -110,36 +110,36 @@ class State:
                     # self.showBoard()
                     # ended with p1 either win or draw
                     self.giveReward()
-                    self.p1.reset()
-                    self.p2.reset()
+                    self.agent.reset()
+                    self.opponent.reset()
                     self.reset()
                     break
 
                 else:
                     # Player 2
                     positions = self.availablePositions()
-                    p2_action = self.p2.chooseAction(positions, self.board, self.playerSymbol)
+                    p2_action = self.opponent.chooseAction(positions, self.board, self.playerSymbol)
                     self.updateState(p2_action)
                     board_hash = self.getHash()
-                    self.p2.addState(board_hash)
+                    self.opponent.addState(board_hash)
 
                     win = self.winner()
                     if win is not None:
                         # self.showBoard()
                         # ended with p2 either win or draw
                         self.giveReward()
-                        self.p1.reset()
-                        self.p2.reset()
+                        self.agent.reset()
+                        self.opponent.reset()
                         self.reset()
                         break
-        self.p1.savePolicy()
+        self.agent.savePolicy()
 
     # play with human
     def play2(self):
         while not self.isEnd:
             # Player 1
             positions = self.availablePositions()
-            p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
+            p1_action = self.agent.chooseAction(positions, self.board, self.playerSymbol)
             # take action and upate board state
             self.updateState(p1_action)
             self.showBoard()
@@ -147,7 +147,7 @@ class State:
             win = self.winner()
             if win is not None:
                 if win == 1:
-                    print(self.p1.name, "wins!")
+                    print(self.agent.name, "wins!")
                 else:
                     print("tie!")
                 self.reset()
@@ -156,14 +156,14 @@ class State:
             else:
                 # Player 2
                 positions = self.availablePositions()
-                p2_action = self.p2.chooseAction(positions)
+                p2_action = self.opponent.chooseAction(positions)
 
                 self.updateState(p2_action)
                 self.showBoard()
                 win = self.winner()
                 if win is not None:
                     if win == -1:
-                        print(self.p2.name, "wins!")
+                        print(self.opponent.name, "wins!")
                     else:
                         print("tie!")
                     self.reset()
@@ -224,11 +224,11 @@ class Player:
 
     # at the end of game, backpropagate and update states value
     def feedReward(self, reward):
-        for st in reversed(self.states):
-            if self.states_value.get(st) is None:
-                self.states_value[st] = 0
-            self.states_value[st] += self.lr * (self.decay_gamma * reward - self.states_value[st])
-            reward = self.states_value[st]
+        for state in reversed(self.states):
+            if self.states_value.get(state) is None:
+                self.states_value[state] = 0
+            self.states_value[state] += self.lr * (self.decay_gamma * reward - self.states_value[state])
+            reward = self.states_value[state]
 
     def reset(self):
         self.states = []
@@ -270,12 +270,12 @@ class HumanPlayer:
 
 if __name__ == "__main__":
     # training
-    # p1 = Player("Jack")
-    # p2 = Player("Rio")
-    #
-    # st = State(p1, p2)
-    # print("training...")
-    # st.play(50000)
+    p1 = Player("Jack")
+    p2 = Player("Rio")
+
+    st = State(p1, p2)
+    print("training...")
+    st.play(50000)
 
     # play with human
     p1 = Player("AI", exp_rate=0)
